31c38453869cd97ea7a64d1bb5f34f3ec26048d1
Paul Smith <mail@paulsmith.site>
2025-02-13 10:59:34 +0000

    adds edge page route

--- app/routes.js
+++ app/routes.js
@@ -17,5 +17,10 @@ router.post('/version-1/:view?', (req, res) => {
   res.redirect(res.locals.paths.next)
 })
 
+// if edge page requested anywhere in this app render the edge.html page
+router.get(/\/edge(\/|$)/, (req, res) => {
+  res.render('edge.html')
+})
+
 // Keep the following line to return your routes to the app
 export default router
