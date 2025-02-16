d3e71d2d80ec9607fa87e2dc78c86ef417078d1e
Paul Smith <mail@paulsmith.site>
2025-02-16 07:11:19 +0000

    search a-z page

--- app/journeys.js
+++ app/journeys.js
@@ -8,6 +8,11 @@ import { wizard } from '../lib/wizard.js'
  */
 export function version1(req) {
   const journey = {
+    '/version-1/where-are-you-now': {},
+    '/version-1/age': {},
+    '/version-1/sex': {},
+    '/version-1/categories/index': {},
+    '/version-1/weirdq': {},
     '/version-1/cause': {},
     '/version-1/timeframe': {},
     '/version-1/how-many-times': {},
@@ -16,11 +21,7 @@ export function version1(req) {
     '/version-1/temperature': {},
     '/version-1/change-in-wound': {},
     '/version-1/do-these-apply': {},
-    '/version-1/disposition': {},
-    '/version-1/where-are-you-now': {},
-    '/version-1/age': {},
-    '/version-1/sex': {},
-    '/version-1/categories': {}
+    '/version-1/disposition': {}
   }
   return wizard(journey, req)
 }
